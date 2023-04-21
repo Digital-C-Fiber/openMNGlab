6da95c97607f300659c6862a3dfd87574c13ce12
Peter Konradi <codingchipmunk@posteo.net>
2023-04-21 10:17:09 +0200

    [planning] base: add method to verify function arguments

--- openmnglab/planning/base.py
+++ openmnglab/planning/base.py
@@ -1,10 +1,11 @@
 from abc import ABC
 from dataclasses import dataclass
-from typing import Optional
+from typing import Collection
 
+from openmnglab.datamodel.exceptions import DataSchemeCompatibilityError
 from openmnglab.datamodel.interface import IDataScheme
 from openmnglab.functions.interface import IFunctionDefinition
-
+from openmnglab.planning.exceptions import InvalidFunctionArgumentCountError, FunctionArgumentSchemaError, PlanningError
 from openmnglab.planning.interface import IExecutionPlan, IExecutionPlanner, IPlannedFunction, IProxyData
 
 
@@ -31,4 +32,20 @@ class PlannerBase(IExecutionPlanner, IExecutionPlan, ABC):
     def get_plan(self) -> ExecutionPlan:
         return ExecutionPlan(self.functions.copy(), self.proxy_data.copy())
 
+    def verify_args(self, function: IFunctionDefinition, *input: IProxyData):
+        for pos, inp in enumerate(input):
+            if inp.calculated_hash not in self.proxy_data:
+                raise PlanningError(
+                    f"Argument at position {pos} with hash {inp.calculated_hash} is not part of this plan and therefore cannot be used as an argument in it")
+        self._check_input(tuple(function.consumes), tuple(inp.schema for inp in input))
 
+    @staticmethod
+    def _check_input(expected_schemes: Collection[IDataScheme], actual_schemes: Collection[IDataScheme]):
+        if len(expected_schemes) != len(actual_schemes):
+            raise InvalidFunctionArgumentCountError(len(expected_schemes), len(actual_schemes))
+        for pos, (expected_scheme, actual_scheme) in enumerate(zip(expected_schemes, actual_schemes)):
+            try:
+                if not expected_scheme.is_compatible(actual_scheme):
+                    raise DataSchemeCompatibilityError("Expected scheme is not compatible with actual scheme")
+            except DataSchemeCompatibilityError as ds_compat_err:
+                raise FunctionArgumentSchemaError(pos) from ds_compat_err
