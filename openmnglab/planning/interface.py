33ca2e1928e25a9fb36213bafb0da46e8d1f669d
Peter Konradi <codingchipmunk@posteo.net>
2023-04-21 13:33:02 +0200

    [planning] add_stage and add_source

--- openmnglab/planning/interface.py
+++ openmnglab/planning/interface.py
@@ -30,6 +30,13 @@ class IExecutionPlanner(ABC):
     def add_function(self, function: IFunctionDefinition, *inp_data: IProxyData) -> Optional[tuple[IProxyData, ...]]:
         ...
 
+    def add_source(self, function: IFunctionDefinition) -> tuple[IProxyData, ...]:
+        return self.add_function(function)
+
+    def add_stage(self, function: IFunctionDefinition, input_0: IProxyData, *other_inputs: IProxyData) -> tuple[
+        IProxyData, ...]:
+        return self.add_function(function, input_0, *other_inputs)
+
     @abstractmethod
     def get_plan(self) -> IExecutionPlan:
         ...
