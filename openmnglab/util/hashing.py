91d2538dbd418def9cb35b0100401d1b380138a5
Peter Konradi <codingchipmunk@posteo.net>
2023-06-21 15:17:09 +0200

    [hash] add functions for float, quantity and bool

--- openmnglab/util/hashing.py
+++ openmnglab/util/hashing.py
@@ -3,11 +3,15 @@ import struct
 from array import array
 from mmap import mmap
 from typing import Self
+try:
+    from quantities import Quantity
+except ImportError as _:
+    Quantity = None
 
 
 class Hash:
     def __init__(self):
-        self.hash = hashlib.sha3_224()
+        self._hash = hashlib.sha3_224()
 
     def str(self, s: str) -> Self:
         self.update(s.encode("UTF8"))
@@ -17,9 +21,28 @@ class Hash:
         self.update(struct.pack("<q", i))
         return self
 
+    def float(self, f: float) -> Self:
+        self.update(struct.pack("<d", f))
+        return self
+
     def update(self, b: bytes | bytearray | memoryview | array | mmap) -> Self:
-        self.hash.update(b)
+        self._hash.update(b)
         return self
 
     def digest(self) -> bytes:
-        return self.hash.digest()
+        return self._hash.digest()
+
+    def quantity(self, q: Quantity) -> Self:
+        self.float(q.magnitude.item())
+        self.str(str(q.units))
+        return self
+
+    def bool(self, b: bool) -> Self:
+        int_repr = int(b)
+        self.update(struct.pack("<q", int_repr))
+        return self
+
+
+
+
+
