e7fb57fb22a7b81e492c7c0f43f3f99b764028df
Peter Konradi <codingchipmunk@posteo.net>
2023-07-05 12:58:52 +0200

    docs for dapsys reader

--- openmnglab/functions/input/readers/dapsys_reader.py
+++ openmnglab/functions/input/readers/dapsys_reader.py
@@ -13,11 +13,30 @@ from openmnglab.util.hashing import Hash
 
 
 class DapsysReader(SourceFunctionDefinitionBase[IProxyData[pd.Series], IProxyData[pd.Series], IProxyData[pd.Series]]):
+    """Loads data from a DAPSYS file
+
+    In: nothing
+
+    Out: [Continuous recording, Stimuli list, tracks]
+
+    Produces
+    ........
+        * Continuous Recording: continuous recording from the file. timestamps as float index, signal values as float
+          values pd.Series[[TIMESTAMP: float], float].
+        * Stimuli list: list of stimuli timestamps. Indexed by the global stimulus id
+          (the stimulus id amongst all stimuli in the file), the label of stimulus and the id of the stimulus type / label
+          (the id amongst all other stimuli in the file which have the same label):
+          pd.Series[[GLOBAL_STIM_ID: int, STIM_TYPE: str, STIM_TYPE_ID: int], float]
+        * tracks: List of all sorted tracks. Indexed by the global stimulus id they are attributed to, the name of the track and their id respective to the track.
+          pd.Series[[GLOBAL_STIM_ID: int, TRACK: str, TRACK_SPIKE_IDX: int], float]
+
+
+
+    """
     def __init__(self, file: str | Path, stim_folder: str, main_pulse: Optional[str] = "Main Pulse",
                  continuous_recording: Optional[str] = "Continuous Recording", responses="responses",
                  tracks: Optional[Sequence[str] | str] = "all"):
         """
-        Create a new DapsysReader function definition
         :param file: Path to the DAPSYS file
         :param stim_folder: The stimulator folder inside the DAPSYS file (i.e. "NI Pulse Stimulator")
         :param main_pulse: Name of the main pulse, defaults to "Main Pulse"
