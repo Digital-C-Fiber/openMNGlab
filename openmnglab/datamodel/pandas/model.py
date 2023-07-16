49c42554de3603bce6ceee07ecec2208fab8faad
Peter Konradi <codingchipmunk@posteo.net>
2023-07-16 11:07:00 +0200

    [bugfix][pandas] assure that all columns of the datastructure have a unit assigned to them

--- openmnglab/datamodel/pandas/model.py
+++ openmnglab/datamodel/pandas/model.py
@@ -11,11 +11,39 @@ from openmnglab.datamodel.exceptions import DataSchemeCompatibilityError, DataSc
 from openmnglab.model.datamodel.interface import IDataContainer, IInputDataScheme, IOutputDataScheme, \
     IStaticDataScheme
 from openmnglab.datamodel.pandas.verification import compare_schemas
+from openmnglab.util.pandas import pandas_names
 
 TPandas = TypeVar('TPandas', pd.Series, pd.DataFrame)
 
 
 class PandasContainer(IDataContainer[TPandas], Generic[TPandas]):
+
+    @staticmethod
+    def check_all_indexes_named(idx: pd.Index | pd.MultiIndex):
+        if isinstance(idx, pd.MultiIndex):
+            for level_i, level_name in enumerate(idx.names):
+                if not level_name:
+                    raise KeyError(f"Multiindex level {level_i} is not named")
+        elif isinstance(idx, pd.Index):
+            if not idx.name:
+                raise KeyError("Index is not named")
+        else:
+            raise TypeError("Passed object is neither a pandas index nor a multiindex")
+
+    @classmethod
+    def check_all_named(cls, data: TPandas):
+        cls.check_all_indexes_named(data.index)
+        if isinstance(data, pd.Series):
+            if not data.name:
+                raise KeyError("Series not named")
+        elif isinstance(data, pd.DataFrame):
+            for col_i, col_name in enumerate(data.columns):
+                if not col_name:
+                    raise KeyError(f"Column {col_i} of data frame is not named")
+        else:
+            raise TypeError("Passed object is neither a pandas dataframe nor a series")
+
+
 
     def __init__(self, data: TPandas, units: dict[str, pq.Quantity]):
         if not isinstance(data, (pd.Series, pd.DataFrame)):
@@ -27,7 +55,11 @@ class PandasContainer(IDataContainer[TPandas], Generic[TPandas]):
                     f"Key {k} in the 'units' dictionary is of type {type(k).__qualname__}, but must be of type {str} or a subtype thereof. ")
             if not isinstance(v, pq.Quantity):
                 raise TypeError(
-                    f"Value of key {k} in the 'units' dictionary is of type {type(v).__qualname__}, but must be of type {pq.Quantity.__qualname__} or a subtype thereof. ")
+                    f"Value of key {k} in the 'units' dictionary is of type {type(v).__qualname__}, but must be of type {pq.Quantity.__qualname__} or a subtype thereof.")
+        self.check_all_named(data)
+        for col_name in pandas_names(data):
+            if col_name not in units:
+                raise KeyError(f"No quantity for element \'{col_name}\' in unit dict")
         self._data = data
         self._units = units
 
--- openmnglab/util/pandas.py
+++ openmnglab/util/pandas.py
@@ -22,6 +22,21 @@ def index_names(inp: pd.Index | pd.MultiIndex) -> Iterable[str]:
     else:
         raise TypeError("Passed index is neither a pandas index nor a pandas multiindex")
 
+
+def pandas_names(inp: pd.Series | pd.DataFrame) -> Iterable[str]:
+    """Iterates over all index and column names of the passed series or dataframe.
+    :param inp: Series or dataframe to yield the names from
+    :return: An iterable of all index and column names
+    """
+    yield from index_names(inp.index)
+    if isinstance(inp, pd.Series):
+        yield inp.name
+    elif isinstance(inp, pd.DataFrame):
+        yield from inp.columns
+    else:
+        raise TypeError("Passed index is neither a pandas series nor a pandas dataframe")
+
+
 def iterdfcols(inp: pd.DataFrame) -> Iterable[tuple[str, pd.Series]]:
     for col_name in inp.columns:
         yield col_name, inp[col_name]
