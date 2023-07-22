ca3b0cb7cd94156041323fdd1e97969f2638145a
Peter Konradi <codingchipmunk@posteo.net>
2023-07-22 15:25:51 +0200

    [st-exec] refactor funcs to static methods

--- openmnglab/execution/singlethreaded.py
+++ openmnglab/execution/singlethreaded.py
@@ -9,20 +9,6 @@ from openmnglab.model.planning.plan.interface import IExecutionPlan, IPlannedDat
 from openmnglab.util.iterables import ensure_iterable
 
 
-def _func_setinput(func: IFunction, *inp: IDataContainer):
-    try:
-        return func.set_input(*inp)
-    except Exception as e:
-        raise FunctionInputError("failed to set input of function") from e
-
-
-def _func_exec(func: IFunction) -> Iterable[IDataContainer]:
-    try:
-        return ensure_iterable(func.execute(), IDataContainer)
-    except Exception as e:
-        raise FunctionExecutionError("function failed to execute") from e
-
-
 class SingleThreadedExecutor(IExecutor):
     def __init__(self):
         self._data: dict[bytes, IDataContainer] = dict()
@@ -34,16 +20,33 @@ class SingleThreadedExecutor(IExecutor):
     def has_computed(self, proxy_data: IProxyData) -> bool:
         return proxy_data.calculated_hash in self._data
 
-    def run_stage(self, stage: IStage):
-        """Executes a single stage and stores its output.
-        
-        .. warn:: Caller must ensure that required input data of the stage is present in :attr:`data`
+    @staticmethod
+    def _set_func_input(func: IFunction, *inp: IDataContainer):
+        """Sets the input of the function and throws a descriptive exception when it fails"""
+        try:
+            return func.set_input(*inp)
+        except Exception as e:
+            raise FunctionInputError("failed to set input of function") from e
 
+    @staticmethod
+    def _exec_func(func: IFunction) -> Iterable[IDataContainer]:
+        """Executes the function and ensures that return is iterable.
+        Also throws a descriptive exception when it fails
+        """
+        try:
+            return ensure_iterable(func.execute(), IDataContainer)
+        except Exception as e:
+            raise FunctionExecutionError("function failed to execute") from e
+
+    def compute_stage(self, stage: IStage):
+        """Runs the function a stage and stores it output.
+
+        .. warn:: Caller must ensure that required input data of the stage is present in :attr:`~.data`
         """
         input_values = tuple(self._data[dependency.calculated_hash] for dependency in stage.data_in)
         func = stage.definition.new_function()
-        _func_setinput(func, *input_values)
-        results: tuple[IDataContainer] = tuple(_func_exec(func))
+        self._set_func_input(func, *input_values)
+        results: tuple[IDataContainer] = tuple(self._exec_func(func))
         if len(results) != len(stage.data_out):
             raise FunctionReturnCountMissmatch(expected=len(stage.data_out), actual=len(results))
         for planned_data_output, actual_data_output in zip(stage.data_out, results):
@@ -52,7 +55,8 @@ class SingleThreadedExecutor(IExecutor):
             # planned_data_output.schema.validate(actual_data_output)
             self._data[planned_data_output.calculated_hash] = actual_data_output
 
-    def execute(self, plan: IExecutionPlan):
+    def execute(self, plan: IExecutionPlan, ignore_previous=False):
         for stage in sorted(plan.stages.values(), key=lambda x: x.depth):
-            if not all(planned_output.calculated_hash in self._data for planned_output in stage.data_out):
-                self.run_stage(stage)
+            if ignore_previous or not all(
+                    planned_output.calculated_hash in self._data for planned_output in stage.data_out):
+                self.compute_stage(stage)
