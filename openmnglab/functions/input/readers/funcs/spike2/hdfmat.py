95d45789ce7974b365a562fa82fd4c9e9ac039ce
Peter Konradi <codingchipmunk@posteo.net>
2023-09-08 12:48:42 +0200

    [spike2-rd] hdf mat: allow for selective loading of ranges

--- openmnglab/functions/input/readers/funcs/spike2/hdfmat.py
+++ openmnglab/functions/input/readers/funcs/spike2/hdfmat.py
@@ -1,3 +1,4 @@
+from itertools import chain
 from typing import Mapping, Iterator
 
 import h5py
@@ -9,18 +10,25 @@ class HDFMatGroup(Mapping):
         self.h5group = h5group
 
     @staticmethod
-    def _parse_dataset(h5ds: h5py.Dataset) -> np.ndarray | tuple[str]:
+    def _parse_dataset(h5ds: h5py.Dataset, *slicers: slice) -> np.ndarray | tuple[str]:
+        assert (len(slicers) <= len(h5ds.shape))
+        
+        def transposed_slicers(norm_slicers: tuple[slice], total_levels: int):
+            # when we transpose a matrix, we have to reverse the slicers for it (since the last level will be returned as the first one)
+            return tuple(chain((slice(None, None, None) for _ in range(total_levels - len(norm_slicers))),
+                               reversed(norm_slicers)))
+
         mat_class = h5ds.attrs.get("MATLAB_class").decode("utf-8")
         if h5ds.attrs.get("MATLAB_empty", 0) == 1:
             if mat_class == "char":
                 return tuple()
             return np.empty(tuple(0 for _ in h5ds.shape), dtype=h5ds.dtype)
         elif mat_class == "char":
-            transposed = h5ds[()].transpose()
+            transposed = h5ds[transposed_slicers(slicers, len(h5ds.shape))].transpose()
             return tuple(by.tobytes().decode("utf-16").split("\x00", 1)[0] for by in transposed)
         if len(h5ds.shape) > 1 and h5ds.shape[0] > 1:
-            return h5ds[()].transpose()
-        return h5ds[()]
+            return h5ds[transposed_slicers(slicers, len(h5ds.shape))].transpose()
+        return h5ds[slicers]
 
     def __len__(self) -> int:
         return self.h5group.__len__()
@@ -28,11 +36,11 @@ class HDFMatGroup(Mapping):
     def __iter__(self) -> Iterator:
         return iter(self.h5group.keys())
 
-    def get(self, key, default=None):
+    def get(self, key, *slicers: slice, default=None):
         dataset = self.h5group.get(key, default=None)
         if dataset is None:
             return default
-        return self._parse_dataset(dataset)
+        return self._parse_dataset(dataset, *slicers)
 
     def __contains__(self, item):
         return self.h5group.__contains__(item)
