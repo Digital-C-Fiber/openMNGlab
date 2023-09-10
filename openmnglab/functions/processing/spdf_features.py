375dcb65c2846832e70fe83a1672a53a6810f088
Peter Konradi <codingchipmunk@posteo.net>
2023-09-10 12:36:14 +0200

    cleanup spdf features

--- openmnglab/functions/processing/spdf_features.py
+++ openmnglab/functions/processing/spdf_features.py
@@ -3,27 +3,26 @@ from abc import ABC
 import pandera as pa
 from pandas import DataFrame
 
-from openmnglab.datamodel.pandas.model import PanderaContainer, PandasDataSchema
+from openmnglab.datamodel.pandas.model import PanderaContainer, PandasDataSchema, DefaultPandasSchemaAcceptor
 from openmnglab.datamodel.pandas.verification import compare_index
 from openmnglab.functions.base import FunctionDefinitionBase
 from openmnglab.functions.processing.funcs.spdf_features import SPDF_FEATURES, FeatureFunc
-from openmnglab.functions.processing.interval_data import IntervalDataAcceptor, IntervalDataOutputSchema
+from openmnglab.functions.processing.interval_data import IntervalDataAcceptor, IntervalDataSchema
 from openmnglab.functions.processing.waveform_components import PrincipleComponentsInputSchema, \
     PrincipleComponentsDynamicOutputSchema
 from openmnglab.model.datamodel.interface import IDataSchema
 from openmnglab.model.planning.interface import IProxyData
 
 
-class SPDFFeaturesBaseSchema(PanderaContainer[pa.DataFrameSchema], ABC):
-    def __init__(self):
+class SPDFFeaturesAcceptor(DefaultPandasSchemaAcceptor[pa.DataFrameSchema], ABC):
+    def __init__(self, index=None):
         super().__init__(pa.DataFrameSchema({
-            feature: pa.Column(float, nullable=True) for feature in SPDF_FEATURES}, title="Principle Components"))
+            feature: pa.Column(float, nullable=True) for feature in SPDF_FEATURES}, title="Principle Components", index=index))
 
 
-class SPDFFeatureOutputSchema(SPDFFeaturesBaseSchema, PandasDataSchema):
+class SPDFFeaturesSchema(PandasDataSchema, SPDFFeaturesAcceptor):
     def __init__(self, idx: pa.Index | pa.MultiIndex):
-        super().__init__()
-        self.pandera_schema.index = idx
+        super().__init__(index=idx)
 
 
 class SPDFFeatures(FunctionDefinitionBase[IProxyData[DataFrame]]):
@@ -43,9 +42,9 @@ class SPDFFeatures(FunctionDefinitionBase[IProxyData[DataFrame]]):
         return PrincipleComponentsInputSchema(), IntervalDataAcceptor(0, 1, 2)
 
     def production_for(self, principle_compo: PrincipleComponentsDynamicOutputSchema,
-                       diffs: IntervalDataOutputSchema) -> SPDFFeatureOutputSchema:
+                       diffs: IntervalDataSchema) -> SPDFFeaturesSchema:
         compare_index(principle_compo.pandera_schema.index, pa.MultiIndex(diffs.pandera_schema.index.indexes[:-1]))
-        return SPDFFeatureOutputSchema(principle_compo.pandera_schema.index)
+        return SPDFFeaturesSchema(principle_compo.pandera_schema.index)
 
     def new_function(self) -> FeatureFunc:
         return FeatureFunc()
